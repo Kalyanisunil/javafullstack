fd2459f9a6435fc0863f48a70e3cd078dfd3a1ca
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-08-13 12:08:21 +0530

    while loops

--- basics/forLoops.java
+++ basics/forLoops.java
@@ -18,6 +18,30 @@ public class forLoops {
 			System.out.println(i);	
 		}
 		
+		
+		System.out.println("PRINTING 1-10  WITH WHILE LOOP: ");	
+		
+		int i=1;
+		while(i<=n)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+		
+		
+		System.out.println("PRINTING 10-1  WITH WHILE LOOP: ");	
+		int j=10;
+		while(j>=1)
+		{
+			System.out.println(j);
+			j--;
+		}
+		
+		
+	
+		
+		
 	}
 
 }
