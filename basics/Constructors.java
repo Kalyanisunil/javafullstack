51ced48cbe6e6776ea8f609f80a478efa1b66b41
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-08-25 11:52:57 +0530

    Constructors

---
+++ basics/Constructors.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Constructors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		defaultConstructor obj= new defaultConstructor();
+		System.out.println(obj.name);
+	
+		
+	}
+
+}
---
+++ basics/defaultConstructor.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class defaultConstructor {
+
+	String name="";
+	defaultConstructor()
+	{
+		name="Kalyani";
+		System.out.println("Called");
+		
+	}
+	
+}
